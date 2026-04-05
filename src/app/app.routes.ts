43608aec0e086834a26671e760edfbac90441e13
HalaMedhatt <halamedhat2486@gmail.com>
2026-04-05 08:44:24 +0200

    fix:confilects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,64 @@
 // import { CenterEdit } from './pages/centers/center-edit/center-edit/center-edit';
 import { Routes } from '@angular/router';
 import { CreateQuizComponent } from './pages/Quizzes/CreateQuiz/create-quiz/create-quiz';
+import { authGuard } from './core/guards/auth guards/auth.guard';
+import { TeacherProfileComponent } from './pages/teacher-profile/teacher-profile';
+import { ProfileComponent } from './pages/profile/profile';
 
 export const routes: Routes = [
+   
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+   path: 'home',
+   loadComponent: () =>
+     import('./pages/home/home').then(m => m.Home)
+ },
     {
+    path: 'profile',
+    component: ProfileComponent,
+    // canActivate: [authGuard],
+    title: 'My Profile — EduCore',
+  },
+  {
+    path: 'teachers/:id',
+    component: TeacherProfileComponent,
+    title: 'Teacher Profile — EduCore',
+  },
+  {
+    path: 'centers/:id',
+    loadComponent: () =>
+      import('./pages/centers/center-detail/center-detail/center-detail')
+        .then(m => m.CenterDetail)
+  },
+
+
+  {
+  path: 'centers/:id/edit',
+  loadComponent: () =>
+    import('./pages/centers/center-edit/center-edit/center-edit')
+      .then(m => m.CenterEdit)
+},
+{
+  path: 'centers/:id/delete',
+  loadComponent: () =>
+    import('./pages/centers/center-delete/center-delete')
+      .then(m => m.CenterDelete)
+},
+
+{
+  path: 'centers/:id/logo',
+  loadComponent: () =>
+    import('./pages/centers/center-logo/center-logo')
+      .then(m => m.CenterLogo)
+},
+{
         path: 'teacher/courses/:courseId/quizzes',
          loadComponent: () =>CreateQuizComponent
   },
-  
-import { authGuard } from './core/guards/auth guards/auth.guard';
-
-export const routes: Routes = [
   // Auth Routes
   {
     path: 'login',
@@ -57,71 +105,10 @@ export const routes: Routes = [
   path: 'teacher/courses/:id/sections',
   canActivate: [authGuard],
   loadComponent: () =>
-    import('./features/courses/pages/teacher/course-sections/course-sections.component')
+    import('./pages/teacher/course-sections/course-sections.component')
       .then(m => m.CourseSectionsComponent)
-},
-  // Default
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  }
-];
-import { TeacherProfileComponent } from './pages/teacher-profile/teacher-profile';
-import { ProfileComponent } from './pages/profile/profile';
-import { Home } from './pages/home/home';
+    },
 
-export const routes: Routes = [
-    { path: '', component: Home },
-    {
-    path: 'profile',
-    component: ProfileComponent,
-    // canActivate: [authGuard],
-    title: 'My Profile — EduCore',
-  },
-  {
-    path: 'teachers/:id',
-    component: TeacherProfileComponent,
-    title: 'Teacher Profile — EduCore',
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-   {
-    path: 'home',
-    loadComponent: () =>
-      import('./pages/home/home').then(m => m.Home)
-  },
-  {
-    path: 'centers/:id',
-    loadComponent: () =>
-      import('./pages/centers/center-detail/center-detail/center-detail')
-        .then(m => m.CenterDetail)
-  },
-
-
-  {
-  path: 'centers/:id/edit',
-  loadComponent: () =>
-    import('./pages/centers/center-edit/center-edit/center-edit')
-      .then(m => m.CenterEdit)
-},
-
-{
-  path: 'centers/:id/delete',
-  loadComponent: () =>
-    import('./pages/centers/center-delete/center-delete')
-      .then(m => m.CenterDelete)
-},
-
-{
-  path: 'centers/:id/logo',
-  loadComponent: () =>
-    import('./pages/centers/center-logo/center-logo')
-      .then(m => m.CenterLogo)
-}
 // ,
 //   {
 //     path: 'centers/:centerId/categories',
