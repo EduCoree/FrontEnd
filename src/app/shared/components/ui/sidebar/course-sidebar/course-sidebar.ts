d70589ef40726d1739fbbb9600c1a8b5516fe2f6
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-04-14 20:54:26 +0200

    Refactor: make the course management have sidebar for quizzes and course info

--- src/app/shared/components/ui/sidebar/course-sidebar/course-sidebar.ts
+++ src/app/shared/components/ui/sidebar/course-sidebar/course-sidebar.ts
@@ -1,12 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-course-sidebar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './course-sidebar.html',
   styleUrl: './course-sidebar.css',
 })
 export class CourseSidebar {
- isOpen = false;
+  isOpen = signal(true);
+  courseId: string | null = null;
+
+  constructor(private route: ActivatedRoute) {
+    // Check both potential param names from your routes.ts
+    this.courseId = this.route.snapshot.params['id'] || 
+                    this.route.snapshot.params['courseId'];
+  }
+
+  // Use a 'get' property so the routes are calculated 
+  // every time they are accessed, ensuring the ID is ready.
+  get navItems() {
+    return [
+      { 
+        labelKey: 'Course Information', 
+        icon: 'business', 
+        route: `/teacher/courses/edit/${this.courseId}`, 
+        exact: true  
+      },
+      { 
+        labelKey: 'Curriculum builder', 
+        icon: 'person_edit', 
+        route: `/teacher/courses/${this.courseId}/sections`, 
+        exact: false 
+      },
+      { 
+        labelKey: 'Quizzes', 
+        icon: 'upload', 
+        route: `/teacher/courses/${this.courseId}/quizzes`, 
+        exact: false 
+      },
+    ];
+  }
+  toggle() {
+    this.isOpen.set(!this.isOpen());
+  }
 }
