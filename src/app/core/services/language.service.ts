2f2511984452ad952bd89d6690bb27f7e4f6ef7b
Menna-mohamed-18 <mennaaboelella18@gmail.com>
2026-04-22 22:29:17 +0200

    feature/translate-3

--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -1,3 +1,35 @@
+// import { Injectable, signal } from '@angular/core';
+// import { TranslateService } from '@ngx-translate/core';
+
+// @Injectable({ providedIn: 'root' })
+// export class LanguageService {
+//   currentLang = signal<'en' | 'ar'>('en');
+
+//   constructor(private translate: TranslateService) {
+//     const saved = localStorage.getItem('lang') as 'en' | 'ar' || 'en';
+//     this.setLanguage(saved);
+//   }
+
+  
+
+//   setLanguage(lang: 'en' | 'ar') {
+//     this.translate.use(lang);
+//     this.currentLang.set(lang);
+//     localStorage.setItem('lang', lang);
+
+
+//     document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
+//     document.documentElement.lang = lang;
+//   }
+
+//   toggle() {
+//     const next = this.currentLang() === 'en' ? 'ar' : 'en';
+//     this.setLanguage(next);
+//   }
+  
+// }
+
+
 import { Injectable, signal } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -14,8 +46,6 @@ export class LanguageService {
     this.translate.use(lang);
     this.currentLang.set(lang);
     localStorage.setItem('lang', lang);
-
-
     document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
     document.documentElement.lang = lang;
   }
@@ -23,5 +53,11 @@ export class LanguageService {
   toggle() {
     const next = this.currentLang() === 'en' ? 'ar' : 'en';
     this.setLanguage(next);
+  }
+
+  setLang(lang: 'en' | 'ar') {
+    if (this.currentLang() !== lang) {
+      this.setLanguage(lang);
+    }
   }
 }
