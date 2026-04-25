ee845a9ee4bc4362d1a52f68bd2eef7240016e0b
Menna-mohamed-18 <mennaaboelella18@gmail.com>
2026-04-25 14:22:23 +0300

    recreate-dashboards+ui

--- src/app/layouts/student-layout/student-layout.component.ts
+++ src/app/layouts/student-layout/student-layout.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+
+
+
+
+
+interface NavItem {
+  route: string;
+  icon: string;
+  labelKey: string;
+  exact?: boolean;
+}
+
+
 
 @Component({
   selector: 'app-student-layout',
@@ -9,4 +22,30 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive , TranslateModule],
   templateUrl: './student-layout.component.html',
 })
-export class StudentLayoutComponent {}
+export class StudentLayoutComponent {
+  private _isOpen = signal(true);
+  
+    isOpen() {
+      return this._isOpen();
+    }
+  
+    toggle() {
+      this._isOpen.update(v => !v);
+    }
+  
+    getToggleIcon(): string {
+      return this._isOpen() ? 'chevron_left' : 'chevron_right';
+    }
+  
+ navItems: NavItem[] = [
+  { route: '/student/dashboard',    icon: 'dashboard',    labelKey: 'studentSidebar.dashboard',    exact: true },
+  { route: '/student/my-courses',   icon: 'menu_book',    labelKey: 'studentSidebar.myCourses' },
+  { route: '/student/quizzes',      icon: 'quiz',         labelKey: 'studentSidebar.myQuizzes' },
+  { route: '/student/quiz-history', icon: 'history',      labelKey: 'studentSidebar.QuizHistory' },
+  { route: '/student/sessions',     icon: 'podcasts',     labelKey: 'studentSidebar.liveSessions' },
+  { route: '/student/certificates', icon: 'verified',     labelKey: 'studentSidebar.certificates' },
+  { route: '/student/messages',     icon: 'chat_bubble',  labelKey: 'studentSidebar.messages' },
+  { route: '/student/settings',     icon: 'settings',     labelKey: 'studentSidebar.settings' },
+  { route: '/student/support',      icon: 'help',         labelKey: 'studentSidebar.support' },
+];
+}
