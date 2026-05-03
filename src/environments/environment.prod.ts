dfb4011bf7ae08fed9730bc05b9e8d65f5711f39
AhmedRady130 <arady6903@gmail.com>
2026-05-03 14:39:47 +0300

    publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://edu-coree.runasp.net'
+  apiUrl: 'https://edu-coree.runasp.net',
+  googleApiKey: 'AIzaSyCvReXB2KMVt6gOD3OBChWg1eAqbZwV2c0',
+  googleClientId: '139604738404-u7vmbri962aips7v3k31h52fsg652pjh.apps.googleusercontent.com',
+  googleProjectNumber: '139604738404',
 };
