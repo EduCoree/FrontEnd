1c3e447a515e0902a711369e58f6a67c85ef2528
AhmedRady130 <arady6903@gmail.com>
2026-05-04 00:11:47 +0300

    fix errors

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
 
-export const environment = {
-  production: true,
-  apiUrl: 'https://edu-coree.runasp.net',
-  googleApiKey: 'AIzaSyCvReXB2KMVt6gOD3OBChWg1eAqbZwV2c0',
-  googleClientId: '139604738404-u7vmbri962aips7v3k31h52fsg652pjh.apps.googleusercontent.com',
-  googleProjectNumber: '139604738404',  // appId for Drive Picker
-};
-
 // export const environment = {
-//   production: false,
-//   apiUrl: 'https://localhost:7275',
+//   production: true,
+//   apiUrl: 'https://edu-coree.runasp.net',
 //   googleApiKey: 'AIzaSyCvReXB2KMVt6gOD3OBChWg1eAqbZwV2c0',
 //   googleClientId: '139604738404-u7vmbri962aips7v3k31h52fsg652pjh.apps.googleusercontent.com',
 //   googleProjectNumber: '139604738404',  // appId for Drive Picker
 // };
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7275',
+  googleApiKey: 'AIzaSyCvReXB2KMVt6gOD3OBChWg1eAqbZwV2c0',
+  googleClientId: '139604738404-u7vmbri962aips7v3k31h52fsg652pjh.apps.googleusercontent.com',
+  googleProjectNumber: '139604738404',  // appId for Drive Picker
+};
